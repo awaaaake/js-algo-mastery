1331ababc2d1ca1b4ac1e90357bcdebb4cf99bac
choyunhee <xornd1110@naver.com>
2024-12-28 18:32:11 +0900

    15654

---
+++ baekjoon/class2/15654/15654.js
@@ -0,0 +1,29 @@
+const input = require("fs")
+  .readFileSync("./baekjoon/class2/example.txt", "utf-8")
+  .toString()
+  .trim()
+  .split("\n");
+const [[N, M], [...numArr]] = input.map((row) => row.split(" ").map(Number));
+numArr.sort((a, b) => a - b);
+const result = [];
+const used = new Array(N).fill(false);
+
+const backtracking = (depth) => {
+  if (depth === M) {
+    console.log(result.join(" "));
+    return;
+  }
+
+  for (let i = 0; i < N; i++) {
+    //start기준 그 다음 수부터 순회x -> 이미 사용된 숫자들을 제외한 모든 수들을 순회
+    if (used[i]) continue;
+
+    result.push(numArr[i]);
+    used[i] = true;
+    backtracking(depth + 1);
+    result.pop();
+    used[i] = false;
+  }
+};
+
+backtracking(0);
