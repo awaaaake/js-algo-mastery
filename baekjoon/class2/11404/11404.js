3c14435ddb771161543670e0c6a12889940aa049
choyunhee <xornd1110@naver.com>
2025-01-14 14:11:12 +0900

    11404

--- baekjoon/class2/11404/11404.js
+++ baekjoon/class2/11404/11404.js
@@ -10,16 +10,18 @@ const m = +input.shift();
 const edgeInfo = input.map((row) => row.split(" ").map(Number));
 const graph = Array.from({ length: n + 1 }, () => Array(n + 1).fill(Infinity));
 
+for (let i = 1; i <= n; i++) {
+  graph[i][i] = 0; // 자기 자신으로 가는 경로는 0
+}
+
 for (let [s, e, w] of edgeInfo) {
   graph[s][e] = Math.min(graph[s][e], w);
 }
-
 
 //폴로이드-워셜 알고리즘
 for (let k = 1; k <= n; k++) {
   for (let i = 1; i <= n; i++) {
     for (let j = 1; j <= n; j++) {
-      if (i === j) graph[i][j] = 0;
       if (graph[i][k] + graph[k][j] < graph[i][j]) {
         graph[i][j] = graph[i][k] + graph[k][j];
       }
@@ -28,6 +30,10 @@ for (let k = 1; k <= n; k++) {
 }
 
 for (let i = 1; i <= n; i++) {
-  console.log(graph[i].slice(1).join(" "));
+  console.log(
+    graph[i]
+      .slice(1)
+      .map((val) => (val === Infinity ? 0 : val))//i에서 j로 갈수없는 경우 0출력
+      .join(" ")
+  );
 }
-
