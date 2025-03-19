f855a49f89ac5cab70196fea90b7664bbd440579
choyunhee <xornd1110@naver.com>
2025-03-19 18:05:35 +0900

    12865

--- baekjoon/12865/12865.js
+++ baekjoon/12865/12865.js
@@ -1,14 +1,15 @@
 const input = require("fs")
-  .readFileSync("./baekjoon/class2/example.txt", "utf-8")
+  .readFileSync("./baekjoon/example.txt", "utf-8")
   .toString()
   .trim()
   .split("\n")
   .map((row) => row.split(" ").map(Number));
 
 const [N, K] = input.shift();
-const dp = Array.from(Array(N + 1), () => Array(K + 1).fill(0));
+const dp = Array.from({ length: N + 1 }, () => new Array(K + 1).fill(0));
 for (let i = 1; i <= N; i++) {
   for (let j = 1; j <= K; j++) {
+    //현재 배낭의 용량
     const [w, v] = input[i - 1];
     dp[i][j] =
       j >= w ? Math.max(dp[i - 1][j], dp[i - 1][j - w] + v) : dp[i - 1][j];
