302cb3971a2c912a9e1451428301dfc6e81e21ca
choyunhee <xornd1110@naver.com>
2025-03-20 16:50:03 +0900

    1647

---
+++ baekjoon/1647/1647.js
@@ -0,0 +1,41 @@
+const input = require("fs")
+  .readFileSync("./baekjoon/example.txt", "utf-8")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((row) => row.split(" ").map(Number));
+
+const [N, M] = input[0];
+const edges = [];
+let parent = Array.from({ length: N + 1 }, (_, i) => i);
+
+const find = (x) => {
+  if (parent[x] === x) return x;
+  return (parent[x] = find(parent[x]));
+};
+
+const union = (a, b) => {
+  const rootA = find(a);
+  const rootB = find(b);
+  if (rootA !== rootB) {
+    a < b ? (parent[rootB] = rootA) : (parent[rootA] = rootB);
+  }
+};
+
+for (let i = 1; i <= M; i++) {
+  const [a, b, cost] = input[i];
+  edges.push([cost, a, b]);
+}
+
+edges.sort((a, b) => a[0] - b[0]);
+let totalCost = 0;
+let maxCost = 0;
+for (let [cost, a, b] of edges) {
+  if (find(a) !== find(b)) {
+    union(a, b);
+    totalCost += cost;
+    maxCost = cost;
+  }
+}
+
+console.log(totalCost - maxCost);
