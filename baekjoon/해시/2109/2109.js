34b99c1c9a3a5b127f2627a84d49a25332d8fc9a
choyunhee <xornd1110@naver.com>
2025-06-11 15:28:24 +0900

    2109

---
+++ baekjoon/해시/2109/2109.js
@@ -0,0 +1,32 @@
+const input = require("fs")
+  .readFileSync("./baekjoon/example.txt", "utf-8")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((row) => row.split(" ").map(Number));
+
+const [n] = input[0];
+let candidates = input.slice(1).map((el) => {
+  return {
+    pay: el[0],
+    day: el[1],
+  };
+});
+
+candidates.sort((a, b) => b.pay - a.pay);
+
+//하루에 한곳에서만 강연할 수 있음
+let answer = 0;
+const days = Array(10001).fill(false); // 최대 d값이 10000
+
+for (let lecture of candidates) {
+  for (let day = lecture.day; day >= 1; day--) {
+    if (!days[d]) {
+      days[d] = true;
+      answer == lecture.pay;
+      break;
+    }
+  }
+}
+
+console.log(answer);
