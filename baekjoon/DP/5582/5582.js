eed368c57035bff6085d722b6316102eadddf17e
choyunhee <xornd1110@naver.com>
2025-08-07 14:55:32 +0900

    5582

--- baekjoon/DP/5582/5582.js
+++ baekjoon/DP/5582/5582.js
@@ -1,9 +1,7 @@
 const input = require("fs")
   .readFileSync("./baekjoon/example.txt", "utf-8")
-  .toString()
   .trim()
-  .split("\n")
-  .map((row) => row.trim());
+  .split("\n");
 
 //가장 긴 공통 부분 문자열 : LCS
 const first = input[0];
@@ -13,11 +11,13 @@ const n = first.length;
 const m = second.length;
 const dp = Array.from({ length: n + 1 }, () => new Array(m + 1).fill(0));
 
+let maxLen = 0;
 for (let i = 1; i <= n; i++) {
   for (let j = 1; j <= m; j++) {
     if (first[i - 1] === second[j - 1]) {
       dp[i][j] = dp[i - 1][j - 1] + 1;
+      maxLen = Math.max(maxLen, dp[i][j]);
     }
   }
 }
-console.log(Math.max(...dp.flat()));
+console.log(maxLen);
