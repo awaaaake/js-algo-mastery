85014499278a205687a1777d6d714e4fd77b990f
choyunhee <xornd1110@naver.com>
2025-09-17 16:28:45 +0900

    1507

--- baekjoon/그래프/1507/1507.js
+++ baekjoon/그래프/1507/1507.js
@@ -14,7 +14,7 @@ let answer = 0;
 for (let k = 0; k < N; k++) {
   for (let i = 0; i < N; i++) {
     for (let j = i; j < N; j++) {
-      //자기 자신으로 가는 최단거리, 중간 노드가 출발점 또는 도착점과 같은 경우는 -> 직접 간선유무를 따질필요x
+      //자기 자신으로 가는 최단거리, 중간 노드가 출발점 또는 도착점과 같은 경우는 -> 직접 간선 유무를 따질필요x
       if (i === j || i === k || j === k) continue;
       if (dist[i][j] > dist[i][k] + dist[k][j]) {
         //거쳐가는 길을 활용하면 되므로, i-j사이에 직접간선이 필요가x -> 도로의 개수가 최소가 아니게 됨
@@ -32,6 +32,7 @@ for (let k = 0; k < N; k++) {
 
 for (let i = 0; i < N; i++) {
   for (let j = i + 1; j < N; j++) {
+    //i->j === j -> i 같은 경로이므로 j는 i+1부터 시작
     if (need[i][j]) answer += dist[i][j];
   }
 }
